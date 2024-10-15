30f0c2e2457671f3043ce544a2fa7ad8c7c2006d
Łukasz Micał <lucas@uigstudio.com>
2024-10-15 13:39:04 +0200

    Update description and author

--- dist/index.js
+++ dist/index.js
@@ -2,8 +2,8 @@
 // @name         Gitlab plus
 // @namespace    https://lukaszmical.pl/
 // @version      2024-10-08
-// @description  try to take over the world!
-// @author       You
+// @description  Gitlab utils
+// @author       Łukasz Micał
 // @match        https://gitlab.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&gitlab.com/
 // ==/UserScript==
